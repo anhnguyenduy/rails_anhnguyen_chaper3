a690edc27f54b738f0d9e2b262c1db98e0368738
anhnguyenduy <thehe9x_153@yahoo.com>
2013-03-20 10:53:31 +0700

    Finish simple title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,16 +3,24 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+	let (:base_title){"Ruby on Rails Tutorial Sample App"}
+
 	  describe "Home page" do
-	  
+	  		
 		  	it "should have content Sample App" do
 		  		visit "/static_pages/home"
 		  		page.should have_selector('h1', :text=> 'Sample App')
 		  	end
 			  	it "should have content title" do
 			  		visit "/static_pages/home"
-			  		page.should have_selector('title', :text=>'Ruby on Rails Tutorial Sample App | Home')
+			  		page.should have_selector('title', :text=>"#{base_title}")
+			  		#page.should have_selector('title' :text=>"#{:base_title} | Home")
 			  	end
+
+			  it "should not have content title" do
+			   visit"/static_pages/home"
+			  	page.should_not have_selector('title', :text=>" | Home")
+			  end	
 
 		  
 	  end
@@ -23,9 +31,14 @@ describe "StaticPages" do
 		  		visit "/static_pages/help"
 		  		page.should have_selector('h1', :text=>'Help')
 				end
-				it "should have content titile" do
+				it "should have content title" do
 					visit "/static_pages/help"
-					page.should have_selector('title', :text=>'Ruby on Rails Tutorial Sample App | Help')
+					page.should have_selector('title', :text=>"#{base_title}")
+				end
+				it "should have not content title" do
+					visit "/static_pages/help"
+					page.should_not have_selector('title' , :text=>" | Help")
+
 				end
 
 			
@@ -38,8 +51,13 @@ describe "StaticPages" do
 			end
 			it "should have content title" do
 				visit "/static_pages/about"
-				page.should have_selector('title', :text=>'Ruby on Rails Tutorial Sample App | About')
+				page.should have_selector('title', :text=>"#{base_title}")
 
+			end
+			it "should not have content title" do
+		
+				visit "/static_pages/about"
+				page.should_not have_selector('title', :text=>" | About")
 			end
 		end	
 end
